dac4d773e86c62d8af2b4b1085a6a82342b9dbe4
axlchan159 <68321858+axlchan159@users.noreply.github.com>
2020-07-15 16:45:14 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,17 +2,15 @@
 
 
 ///Edit Under This
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="/__/firebase/7.16.0/firebase-app.js"></script>
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
-};
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="/__/firebase/7.16.0/firebase-analytics.js"></script>
+
+<!-- Initialize Firebase -->
+<script src="/__/firebase/init.js"></script>
 
      
  
